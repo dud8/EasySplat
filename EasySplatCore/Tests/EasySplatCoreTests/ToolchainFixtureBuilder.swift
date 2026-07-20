a1e0d3ae94894a89071a7875a659227c67d83f71
dud8 <159498121+dud8@users.noreply.github.com>
2026-07-20 14:55:35 -0400

    fix(training): bind timing to root command buffers

--- EasySplatCore/Tests/EasySplatCoreTests/ToolchainFixtureBuilder.swift
+++ EasySplatCore/Tests/EasySplatCoreTests/ToolchainFixtureBuilder.swift
@@ -120,7 +120,7 @@ enum ToolchainFixtureBuilder {
                 "geometry_adam_fusion_patch_sha256": "927ad1fdbffee7ad762396c7acc965cd4a20da781f172240c62aa94f41e1cd2c",
                 "parallel_radix_scan_patch_sha256": "1caedde675063dd0b119e91ec39a6945328ecf37134a83b079dce964a7a816c4",
                 "allocation_pressure_patch_sha256": "d5235770565c75387ad42ec4b534895322275822ab5913d0bc05bcf3bba95083",
-                "exact_prefix_hardening_patch_sha256": "99022e824c91ca57b34f60f21b29753db788290541c3c6bc52a5b496794d9683",
+                "exact_prefix_hardening_patch_sha256": "e7437861085e86e8671898b8c5833568e0420bdf1c0559175ddcdb34ed159ace",
                 "dependencies": [
                     "nlohmann_json_v3.11.3_sha256": "04022b05d806eb5ff73023c280b68697d12b93e1b7267a0b22a1a39ec7578069",
                     "nanoflann_v1.5.5_sha256": "57496cb27e1310a77a367e5a902c8f1c700496d91ac54ccc87fbe9ccc28bc6cc",
